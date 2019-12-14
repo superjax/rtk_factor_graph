674da3e44e9a0bb836610fc75719553e8d9c67ac
James Jackson <superjax08@gmail.com>
2019-12-14 05:57:28 +0000

    make quat const-correct, and improve taylor series handling

--- common/geometry/test_quat.cxx
+++ common/geometry/test_quat.cxx
@@ -6,35 +6,35 @@
 #include <unsupported/Eigen/MatrixFunctions>
 
 #include "common/geometry/quat.h"
+#include "common/matrix_defs.h"
 #include "common/test_helpers.h"
-
-using namespace Eigen;
-using namespace std;
 
 constexpr int NUM_ITERS = 100;
 
 TEST(Quat, quat_rotation_direction)
 {
     // Compare against a known active and passive rotation
-    Vector3d v, beta, v_active_rotated, v_passive_rotated;
-    v << 0, 0, 1;
-    v_active_rotated << 0, -1.0 * std::pow(0.5, 0.5), std::pow(0.5, 0.5);
-    beta << 1, 0, 0;
-    Quatd q_x_45 = Quatd::from_axis_angle(beta, 45.0 * M_PI / 180.0);
-    Eigen::Vector3d v_x_45 = q_x_45.rota(v);
+    const Vec3 v(0, 0, 1);
+    const Vec3 beta(1, 0, 0);
+    Quat<double> q_x_45 = Quat<double>::from_axis_angle(beta, 45.0 * M_PI / 180.0);
+    const Vec3 v_x_45 = q_x_45.rota(v);
+    const Vec3 v_active_rotated(0, -1.0 * std::pow(0.5, 0.5), std::pow(0.5, 0.5));
 
-    Matrix3d R_true;
-    R_true << 1.0000000, 0.0000000, 0.0000000, 0.0000000, 0.70710678118654757, 0.70710678118654757,
-        0.0000000, -0.70710678118654757, 0.70710678118654757;
-    Matrix3d qR = q_x_45.R();
+    // clang-format off
+    Mat3 R_true;
+    R_true << 1.0000000,  0.0000000,           0.0000000,
+              0.0000000,  0.70710678118654757, 0.70710678118654757,
+              0.0000000, -0.70710678118654757, 0.70710678118654757;
+    // clang-format on
+    const Mat3 qR = q_x_45.R();
     MATRIX_EQUALS(qR, R_true);
     MATRIX_EQUALS(qR.transpose() * v, v_active_rotated);
     MATRIX_EQUALS(R_true.transpose() * v, v_active_rotated);
 
     MATRIX_EQUALS(v_x_45, v_active_rotated);
 
-    v_passive_rotated << 0, std::pow(0.5, 0.5), std::pow(0.5, 0.5);
-    Vector3d v_x_45_T = q_x_45.rotp(v);
+    const Vec3 v_passive_rotated(0, std::pow(0.5, 0.5), std::pow(0.5, 0.5));
+    const Vec3 v_x_45_T = q_x_45.rotp(v);
     MATRIX_EQUALS(v_x_45_T, v_passive_rotated);
     MATRIX_EQUALS(qR * v, v_passive_rotated);
     MATRIX_EQUALS(R_true * v, v_passive_rotated);
@@ -42,12 +42,10 @@ TEST(Quat, quat_rotation_direction)
 
 TEST(Quat, quat_rot_invrot_R)
 {
-    Vector3d v;
-    Quatd q1 = Quatd::Random();
     for (int i = 0; i < NUM_ITERS; i++)
     {
-        v.setRandom();
-        q1 = Quatd::Random();
+        const Vec3 v = Vec3::Random();
+        const Quat<double> q1 = Quat<double>::Random();
 
         // Check that rotations are inverses of each other
         MATRIX_EQUALS(q1.rota(v), q1.R().transpose() * v);
@@ -57,27 +55,38 @@ TEST(Quat, quat_rot_invrot_R)
 
 TEST(Quat, quat_from_two_unit_vectors)
 {
-    Vector3d v1, v2;
+    Vec3 v1, v2;
     for (int i = 0; i < NUM_ITERS; i++)
     {
-        v1.setRandom();
-        v2.setRandom();
+        // Check singularity condition
+        if (i < NUM_ITERS / 3)
+        {
+            v1.setRandom();
+            v2 = -v1 + Vec3::Random() * 1e-4;
+        }
+        else
+        {
+            v1.setRandom();
+            v2.setRandom();
+        }
         v1 /= v1.norm();
         v2 /= v2.norm();
 
-        MATRIX_EQUALS(Quatd::from_two_unit_vectors(v1, v2).rota(v1), v2);
-        MATRIX_EQUALS(Quatd::from_two_unit_vectors(v2, v1).rotp(v1), v2);
+        const Quat<double> q = Quat<double>::from_two_unit_vectors(v1, v2);
+
+        MATRIX_EQUALS(q.rotp(v1), v2);
+        MATRIX_EQUALS(q.rota(v2), v1);
     }
 }
 
 TEST(Quat, from_R)
 {
-    Vector3d v;
+    Vec3 v;
     for (int i = 0; i < NUM_ITERS; i++)
     {
-        Quatd q1 = Quatd::Random();
-        Matrix3d R = q1.R();
-        Quatd qR = Quatd::from_R(R);
+        const Quat<double> q1 = Quat<double>::Random();
+        const Mat3 R = q1.R();
+        const Quat<double> qR = Quat<double>::from_R(R);
         v.setRandom();
         MATRIX_EQUALS(qR.rota(v), R.transpose() * v);
         MATRIX_EQUALS(q1.rota(v), R.transpose() * v);
@@ -90,8 +99,8 @@ TEST(Quat, from_R)
 
 TEST(Quat, otimes)
 {
-    Quatd q1 = Quatd::Random();
-    Quatd qI = Quatd::Identity();
+    const Quat<double> q1 = Quat<double>::Random();
+    const Quat<double> qI = Quat<double>::Identity();
     QUATERNION_EQUALS(q1 * q1.inverse(), qI);
 }
 
@@ -100,33 +109,29 @@ TEST(Quat, exp_log_axis_angle)
     // Check that qexp is right by comparing with matrix exp and axis-angle
     for (int i = 0; i < NUM_ITERS; i++)
     {
-        Vector3d omega;
-        omega.setRandom();
-        Matrix3d R_omega_exp_T = Quatd::skew(omega).exp();  // Why is this needed?
-        Quatd q_R_omega_exp = Quatd::from_R(R_omega_exp_T.transpose());
-        Quatd q_omega = Quatd::from_axis_angle(omega / omega.norm(), omega.norm());
-        Quatd q_omega_exp = Quatd::exp(omega);
+        const Vec3 omega = Vec3::Random();
+        const Mat3 R_omega_exp_T = skew(omega).exp();
+        const Quat<double> q_R_omega_exp = Quat<double>::from_R(R_omega_exp_T.transpose());
+        const Quat<double> q_omega =
+            Quat<double>::from_axis_angle(omega / omega.norm(), omega.norm());
+        const Quat<double> q_omega_exp = Quat<double>::exp(omega);
         QUATERNION_EQUALS(q_R_omega_exp, q_omega);
         QUATERNION_EQUALS(q_omega_exp, q_omega);
 
         // Check that exp and log are inverses of each otherprint_error
-        MATRIX_EQUALS(Quatd::log(Quatd::exp(omega)), omega);
-        QUATERNION_EQUALS(Quatd::exp(Quatd::log(q_omega)), q_omega);
+        MATRIX_EQUALS(Quat<double>::exp(omega).log(), omega);
+        QUATERNION_EQUALS(Quat<double>::exp(q_omega.log()), q_omega);
     }
 }
 
 TEST(Quat, inplace_and_mul)
 {
-    Vector3d delta1, delta2, zeros;
-    zeros.setZero();
     for (int i = 0; i < NUM_ITERS; i++)
     {
-        Quatd q = Quatd::Random();
-        Quatd q2 = Quatd::Random();
-        Quatd q_copy = q.copy();
+        const Quat<double> q = Quat<double>::Random();
+        Quat<double> q2 = Quat<double>::Random();
+        Quat<double> q_copy = q.copy();
         QUATERNION_EQUALS(q_copy, q);
-        delta1.setRandom();
-        delta2.setRandom();
 
         q_copy = q.copy();
         q_copy *= q2;
@@ -140,84 +145,86 @@ TEST(Quat, euler)
     std::default_random_engine gen;
     for (int i = 0; i < NUM_ITERS; i++)
     {
-        double roll = M_PI * uni(gen);
-        double pitch = M_PI / 2.0 * uni(gen);
-        double yaw = M_PI * uni(gen);
-        Quatd q = Quatd::from_euler(roll, pitch, yaw);
-        ASSERT_NEAR(roll, q.roll(), 1e-8);
-        ASSERT_NEAR(pitch, q.pitch(), 1e-8);
-        ASSERT_NEAR(yaw, q.yaw(), 1e-8);
-        Quatd q2 = Quatd::from_euler(q.roll(), q.pitch(), q.yaw());
+        const double roll = M_PI * uni(gen);
+        const double pitch = M_PI / 2.0 * uni(gen);
+        const double yaw = M_PI * uni(gen);
+        const Quat<double> q = Quat<double>::from_euler(roll, pitch, yaw);
+
+        EXPECT_NEAR(roll, q.roll(), 1e-8);
+        EXPECT_NEAR(pitch, q.pitch(), 1e-8);
+        EXPECT_NEAR(yaw, q.yaw(), 1e-8);
+        const Quat<double> q2 = Quat<double>::from_euler(q.roll(), q.pitch(), q.yaw());
         QUATERNION_EQUALS(q, q2);
     }
 }
 
 TEST(Quat, passive_rotation_derivative)
 {
-    Quatd q0 = Quatd::Random();
-    Vector3d v;
-    v.setRandom();
+    const Quat<double> q0 = Quat<double>::Random();
+    const Vec3 v = Vec3::Random();
 
-    Matrix3d a;
-    Matrix3d fd;
-    Matrix3d I = Matrix3d::Identity();
-    double epsilon = 1e-8;
+    const Mat3 I = Mat3::Identity();
+    constexpr double epsilon = 1e-8;
 
-    a = skew(q0.rotp(v));  // [R(q)v]
+    const Mat3 a = skew(q0.rotp(v));  // [R(q)v]
+    Mat3 fd;
 
     for (int i = 0; i < 3; i++)
     {
-        Quatd qi = q0 * Quatd::exp(epsilon * (I.col(i)));
+        const Quat<double> qi = q0 * Quat<double>::exp(epsilon * (I.col(i)));
         fd.col(i) = (qi.rotp(v) - q0.rotp(v)) / epsilon;
     }
-    if ((fd - a).array().abs().sum() > 1e-6)
-    {
-        std::cout << "ERROR IN LINE " << __LINE__ << "\nA:\n" << a << "\nD:\nfd" << fd << std::endl;
-    }
-    ASSERT_LE((fd - a).array().abs().sum(), 1e-6);
+    MATRIX_CLOSE(fd, a, 1e-6);
 }
 
 TEST(Quat, active_rotation_derivative)
 {
-    Quatd q0 = Quatd::Random();
-    Vector3d v;
-    v.setRandom();
+    const Quat<double> q0 = Quat<double>::Random();
+    const Vec3 v = Vec3::Random();
 
-    Matrix3d a;
-    Matrix3d fd;
-    Matrix3d I = Matrix3d::Identity();
-    double epsilon = 1e-8;
+    const Mat3 I = Mat3::Identity();
+    const double epsilon = 1e-8;
 
-    a = -q0.R().transpose() * skew(v);  // -R(q).T * [v]
+    const Mat3 a = -q0.R().transpose() * skew(v);  // -R(q).T * [v]
+    Mat3 fd;
 
     for (int i = 0; i < 3; i++)
     {
-        Quatd qi = q0 * Quatd::exp(epsilon * (I.col(i)));
+        const Quat<double> qi = q0 * Quat<double>::exp(epsilon * (I.col(i)));
         fd.col(i) = (qi.rota(v) - q0.rota(v)) / epsilon;
     }
-    if ((fd - a).array().abs().sum() > 1e-6)
-    {
-        std::cout << "ERROR IN LINE " << __LINE__ << "\nA:\n" << a << "\nD:\nfd" << fd << std::endl;
-    }
-    ASSERT_LE((fd - a).array().abs().sum(), 1e-6);
+    MATRIX_CLOSE(fd, a, 1e-6);
 }
 
 TEST(Quat, exp_approx)
 {
+    const Mat3 I = Mat3::Identity();
     for (int j = 0; j < NUM_ITERS; j++)
     {
-        Quatd q = Quatd::Random();
+        Quat<double> q = Quat<double>::Random();
         if (j == 0)
-            q = Quatd::Identity();
-        Vector3d delta;
-        Matrix3d I = Matrix3d::Identity();
+            q = Quat<double>::Identity();
+
+        Vec3 delta;
         delta.setRandom();
         delta *= 0.1;
 
-        Quatd qp = q * Quatd::exp(delta);
+        const Quat<double> qp = q * Quat<double>::exp(delta);
 
-        Matrix3d actual = qp.R();
-        Matrix3d approx = (I - skew(delta)) * q.R();
-        ASSERT_LE((actual - approx).array().abs().sum(), 0.1);
+        const Mat3 actual = qp.R();
+        const Mat3 approx = (I - skew(delta)) * q.R();
+        MATRIX_CLOSE(actual, approx, 0.1);
+    }
+}
+
+TEST(Quat, log_close)
+{
+    for (int i = 0; i < NUM_ITERS; ++i)
+    {
+        const Quat<double> q = Quat<double>::Random();
+        const Vec3 delta = Vec3::Random() * 4e-6;
+        const Quat<double> q2 = q * Quat<double>::exp(delta);
+        const Vec3 delta_recovered = (q.inverse() * q2).log();
+        MATRIX_EQUALS(delta, delta_recovered);
     }
 }
