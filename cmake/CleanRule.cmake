5dfe28c95ba2c5fb486a945dfa69463c109179a3
James Jackson <superjax08@gmail.com>
2020-01-09 03:46:32 +0000

    Clean rule

---
+++ cmake/CleanRule.cmake
@@ -0,0 +1,15 @@
+# This script creates a file called clean in the build directory that removes everything except the
+# third-party downloaded libraries.  This significantly speeds up clean builds
+
+# First, create the script (This will need to be manually updated if we add source code in other
+# directories)
+file(WRITE "${PROJECT_BINARY_DIR}/tmp/clean"
+     "#!/bin/bash\n\n rm -rf client common scratch third_party utils tmp")
+
+# Now, move the file into the build directory, and apply the right permisions.  You can't set the
+# permissions on a new file through CMAKE, so this hack of starting in the /tmp folder, then moving
+# it into the build will work for now
+file(COPY "${PROJECT_BINARY_DIR}/tmp/clean"
+    DESTINATION "${PROJECT_BINARY_DIR}"
+    FILE_PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
+)
