724ca10fbfa79a30d2c816de4105b89752bf7c19
James Jackson <superjax08@gmail.com>
2020-09-12 21:20:43 -0700

    update

--- cmake/PyRule.cmake
+++ cmake/PyRule.cmake
@@ -2,14 +2,14 @@ function(py_executable name src)
     set(DESTINATION_SCRIPT ${CMAKE_CURRENT_BINARY_DIR}/${name})
     set(PYTHON_FILE ${CMAKE_CURRENT_LIST_DIR}/${src})
 
+    # This creates a script that runs the python file after adding the root of this project to the
+    # pythonpath
     add_custom_target(${name} ALL
         COMMAND ${CMAKE_COMMAND} -E echo "#!/bin/bash" > ${DESTINATION_SCRIPT}
         COMMAND ${CMAKE_COMMAND} -E echo "export PYTHONPATH=\$PYTHONPATH:${PROJECT_SOURCE_DIR}" >> ${DESTINATION_SCRIPT}
-        COMMAND ${CMAKE_COMMAND} -E echo "cd ${PROJECT_SOURCE_DIR}" >> ${DESTINATION_SCRIPT}
         COMMAND ${CMAKE_COMMAND} -E echo "python3 ${PYTHON_FILE}" >> ${DESTINATION_SCRIPT}
         COMMAND chmod +x ${DESTINATION_SCRIPT}
         BYPRODUCTS ${DESTINATION_SCRIPT}
-
         VERBATIM
     )
 endfunction()
